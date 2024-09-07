84628a54e0efc8a7c14919b2f5070de962d09bd0
ewernn <eawern@outlook.com>
2024-09-07 15:33:52 -0700

    use 1000x1000 images and just crop them

--- configs/unet/eric-unet-s5-d16_fcn_1xb4-ce-1.0-dice-3.0-40k_CAT_KIDNEY-512x512.py
+++ configs/unet/eric-unet-s5-d16_fcn_1xb4-ce-1.0-dice-3.0-40k_CAT_KIDNEY-512x512.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/fcn_unet_s5-d16-1channel.py', '../_base_/datasets/cat_kidney_grayscale.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 crop_size = (512,512)
 data_preprocessor = dict(size=crop_size)
