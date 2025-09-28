8ac55e249fe34a5a457237ef870342eba1e84cf1
ewernn <eawern@outlook.com>
2025-09-27 23:22:08 -0700

    fixed class labels [0 38 75] --> [0 1 2]

--- mmseg/datasets/cat_kidney.py
+++ mmseg/datasets/cat_kidney.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Optional, Sequence, Union, Dict
 from mmseg.datasets.voc import PascalVOCDataset
 from mmseg.registry import DATASETS
 
@@ -9,8 +10,7 @@ class CatKidneyDataset(PascalVOCDataset):
 
     METAINFO = dict(
         classes=('background', 'left_kidney', 'right_kidney'),
-        palette=[[120, 120, 120], [6, 230, 230], [56, 59, 120]],
-        label_map={0: 0, 38: 1, 75: 2}
+        palette=[[120, 120, 120], [6, 230, 230], [56, 59, 120]]
     )
 
     def __init__(self,
@@ -23,3 +23,11 @@ class CatKidneyDataset(PascalVOCDataset):
             seg_map_suffix=seg_map_suffix,
             reduce_zero_label=reduce_zero_label,
             **kwargs)
+
+    @classmethod
+    def get_label_map(cls, new_classes: Optional[Sequence] = None) -> Union[Dict, None]:
+        """Return custom label map for kidney dataset.
+
+        Remaps mask values from [0, 38, 75] to [0, 1, 2].
+        """
+        return {0: 0, 38: 1, 75: 2}
