26c289bfd4e36d7229551fc9ddd03dc03df75960
ewernn <eawern@outlook.com>
2025-10-01 06:43:49 -0700

    fixed wandb dice logging

--- configs/unet/sweep_config.py
+++ configs/unet/sweep_config.py
@@ -1,11 +1,11 @@
 _base_ = './eric-unet-s5-d16_fcn_1xb4-ce-1.0-dice-3.0-40k_CAT_KIDNEY-512x512.py'
 
-# Override visualizer to remove WandB for sweep compatibility
-visualizer = dict(
-    type='SegLocalVisualizer',
-    vis_backends=[dict(type='LocalVisBackend')],  # Only local, no WandB
-    name='visualizer'
-)
+# Override custom_imports to avoid YAPF parsing errors during sweep
+custom_imports = dict(imports=[], allow_failed_imports=False)
+
+# Disable visualizer completely to avoid YAPF issues
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = None
 
 # Fixed class weights, but loss weights will be overridden by sweep
 model = dict(
